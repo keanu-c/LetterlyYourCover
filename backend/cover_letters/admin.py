dbd28420e95054ef0675f041af5dbc0324e4c8d9
Keanu Chau <keanuchau@gmail.com>
2024-11-03 12:32:31 -0800

    Register models in admin.py

--- backend/cover_letters/admin.py
+++ backend/cover_letters/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CoverLetterTemplate
 # Register your models here.
+@admin.register(CoverLetterTemplate)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name', 'description', 'content')
